ece67144774afe04f58208dbd7342cfcaef6c16a
lukasz341 <lukasz341@tlen.pl>
2017-11-06 23:20:37 +0100

    poprawa ładowania obrazka z z dysku

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ var http = require('http');
 
 var server = http.createServer();
 
-fs.readFile('./index.html', 'utf-8', function(err, data) {
+
 
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.write(data);
+            response.end();
         
-        response.write(data);
-        response.end();
+        });
+        
     } else {
             response.statusCode = 404;
-            response.write('<img src="http://shaebaxter.com/wp-content/uploads/2014/06/404-error-page-example.jpg" alt="obrazek" >');
-            response.end();
+            fs.readFile('images/404error.jpg', function(err, data) {
+                response.writeHead(200, {"Content-Type": "image/jpeg"});
+              
+                response.end(data);
+            });
     }
 });
 
-});
+
 
 server.listen(8080);
 
